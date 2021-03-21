0d0ae4a4163ddb2fad14a7c84d1f9cb7d9dbbb6d
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2021-03-21 15:42:20 +1000

    added copy to clipboard

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import { useSpring, animated } from 'react-spring';
 const Joke = ({ joke, searchTerm }) => {
+  const [copy, setCopy] = useState(false);
   const jokeLength = joke.split(' ').length;
 
+  // for other emphasising solution
   // const search = new RegExp(searchTerm, 'i');
   // const formattedJoke = joke.replace(search, '<span>$&</span>');
+  const copyToClipBoard = () => {
+    navigator.clipboard.writeText(joke);
+    setCopy(true);
+
+    setTimeout(() => {
+      setCopy(false);
+    }, 500);
+  };
 
   return (
-    <StyledJoke>
+    <StyledJoke onClick={copyToClipBoard}>
+      <div className='copied-to-clipboard'>
+        <span>{copy ? 'Copied ' + String.fromCharCode(10003) : 'Copy?'}</span>
+      </div>
+
       {/* // This probaby isn't a safe example? because the api might respond with a malicious script?
       <p
         dangerouslySetInnerHTML={{ __html: formattedJoke }}
@@ -32,7 +46,7 @@ const Joke = ({ joke, searchTerm }) => {
       ) : (
         <p className='joke-text'>{joke}</p>
       )}
-      <p
+      <span
         className='joke-length'
         style={{
           backgroundColor:
@@ -46,7 +60,7 @@ const Joke = ({ joke, searchTerm }) => {
         }}
       >
         {jokeLength < 10 ? 'short' : jokeLength < 20 ? 'medium' : 'long'}
-      </p>
+      </span>
     </StyledJoke>
   );
 };
@@ -60,11 +74,37 @@ const StyledJoke = styled.div`
   padding: 1em 1.5em;
   margin-bottom: 1em;
   color: black;
+  cursor: pointer;
   box-shadow: 0px 3px 10px -5px rgba(194, 192, 194, 1);
+  z-index: 200;
+  .copied-to-clipboard {
+    top: 0px;
+    font-weight: 900;
+    right: 0px;
+    position: absolute;
+    font-size: 14px;
+    width: 70px;
+    text-align: center;
+    margin: 0;
+    border-bottom-left-radius: 5px;
+    border-top-right-radius: 5px;
+    background: black;
+    padding: 10px;
+    color: white;
+    opacity: 0;
+    z-index: 26;
+    transition: opacity 0.2s linear;
+  }
+  &:hover {
+    .copied-to-clipboard {
+      opacity: 1;
+    }
+  }
   .joke-text {
     font-weight: 500;
     width: 70%;
     span {
+      border-radius: 5px;
       color: #eb7434;
       font-weight: 900;
     }
@@ -84,6 +124,7 @@ const StyledJoke = styled.div`
     background: #4387ec;
     padding: 10px;
     color: white;
+    z-index: 25;
   }
 `;
 export default Joke;
